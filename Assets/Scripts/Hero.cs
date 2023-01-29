5630572a3e4eec5bd2e23fbbd5f883a1929c5e61
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-29 19:02:28 +0300

    chg: script Hero

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -25,7 +25,7 @@ namespace PixelCrew
             var isJumping = _direction.y > 0;
             if (isJumping)
             {
-                if (IsGrounded())
+                if (IsGrounded() && _rigidbody.velocity.y <= 0.1f)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
                 }
