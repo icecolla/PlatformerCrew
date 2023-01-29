5ff817cf93382bf348aa6bfbae79a53d63ccd4c4
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-30 00:01:41 +0300

    chg: hero scripts w/ animation

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -7,15 +7,22 @@ namespace PixelCrew
         [SerializeField] private float _speed = 1f;
         [SerializeField] private float _jumpPower = 1f;
 
+        [SerializeField] private LayerCheck _groundCheck;
+
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
 
-        [SerializeField] private LayerCheck _groundCheck;
-
+        private SpriteRenderer _sprite;
+        private Animator _animator;
+        private static readonly int isGroundKey = Animator.StringToHash("is-ground");
+        private static readonly int isRunningKey = Animator.StringToHash("is-running");
+        private static readonly int verticalVelocityKey = Animator.StringToHash("vertical-velocity");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+            _sprite = GetComponent<SpriteRenderer>();
         }
 
         private void FixedUpdate()
@@ -23,9 +30,11 @@ namespace PixelCrew
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
+
             if (isJumping)
             {
-                if (IsGrounded() && _rigidbody.velocity.y <= 0.1f)
+                if (isGrounded && _rigidbody.velocity.y <= 0.1f)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
                 }
@@ -34,6 +43,24 @@ namespace PixelCrew
             else if (_rigidbody.velocity.y > 0)
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+            }
+
+            _animator.SetBool(isRunningKey, _direction.x != 0);
+            _animator.SetBool(isGroundKey, isGrounded);
+            _animator.SetFloat(verticalVelocityKey, _rigidbody.velocity.y);
+
+            UpdateSpriteDirection();
+        }
+
+        private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+            {
+                _sprite.flipX = false;
+            }
+            else if (_direction.x < 0)
+            {
+                _sprite.flipX = true;
             }
         }
 
