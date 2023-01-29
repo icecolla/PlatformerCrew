378195d936b1b5f35cd1eb7ff85b9f8c2ac79b58
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-29 18:06:18 +0300

    chg: Hero script

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -5,22 +5,41 @@ namespace PixelCrew
     public class Hero : MonoBehaviour
     {
         [SerializeField] private float _speed = 1f;
+        [SerializeField] private float _jumpPower = 1f;
 
+        private Rigidbody2D _rigidbody;
         private Vector2 _direction;
 
-        private void Update()
+        [SerializeField] private LayerCheck _groundCheck;
+
+
+        private void Awake()
         {
-            Movement();
+            _rigidbody = GetComponent<Rigidbody2D>();
         }
 
-        public void Movement()
+        private void FixedUpdate()
         {
-            if (_direction.magnitude > 0)
+            _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
+
+            var isJumping = _direction.y > 0;
+            if (isJumping)
             {
-                var delta = _direction * _speed * Time.deltaTime;
-                transform.position += new Vector3(delta.x, delta.y,
-                                                                                                    transform.position.z);
+                if (IsGrounded())
+                {
+                    _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
+                }
+
             }
+            else if (_rigidbody.velocity.y > 0)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return _groundCheck.isTouchingLayer;
         }
 
         public void SetDirection(Vector2 direction)
@@ -30,7 +49,7 @@ namespace PixelCrew
 
         public void SaySomething()
         {
-            Debug.Log("Say something");
+            Debug.Log("Hello Pixel Crew!");
         }
     }
 }
