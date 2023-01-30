e4c24059a2ac35da91357804d7deefca8a1551a9
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-30 14:39:11 +0300

    add: script TeleportComponent

---
+++ Assets/Scripts/Components/TeleportComponent.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PixelCrew.Components
+{
+    public class TeleportComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _destination;
+        [SerializeField] private float _alphaTime = 1f;
+        [SerializeField] private float _moveTime = 1f;
+
+        public void Teleport(GameObject target)
+        {
+            StartCoroutine(AnimateTeleport(target));
+        }
+
+        private IEnumerator AnimateTeleport(GameObject target)
+        {
+            var sprite = target.GetComponent<SpriteRenderer>();
+            var input = target.GetComponent<PlayerInput>();
+
+            SetLockInput(input, true);
+
+            yield return AlphaAnimation(sprite, 0f);
+            target.SetActive(false);
+
+            yield return MoveAnimation(target);
+
+            target.SetActive(true);
+            yield return AlphaAnimation(sprite, 1f);
+            SetLockInput(input, false);
+        }
+
+        private IEnumerator MoveAnimation(GameObject target)
+        {
+            var moveTime = 0f;
+            while (moveTime < _moveTime)
+            {
+                moveTime += Time.deltaTime;
+                var progress = moveTime / _moveTime;
+                target.transform.position = Vector3.Lerp(target.transform.position, _destination.position, progress);
+
+                yield return null;
+            }
+        }
+
+        private IEnumerator AlphaAnimation(SpriteRenderer sprite, float destAlpha)
+        {
+            var time = 0f;
+            var spriteAlpha = sprite.color.a;
+            while (time < _alphaTime)
+            {
+                time += Time.deltaTime;
+
+                float progress = time / _alphaTime;
+                var tmpAlpha = Mathf.Lerp(spriteAlpha, destAlpha, progress);
+                var color = sprite.color;
+                color.a = tmpAlpha;
+                sprite.color = color;
+
+                yield return null;
+            }
+        }
+
+        private void SetLockInput(PlayerInput input, bool isLocked)
+        {
+            if (input != null)
+            {
+                input.enabled = !isLocked;
+            }
+        }
+    }
+}
