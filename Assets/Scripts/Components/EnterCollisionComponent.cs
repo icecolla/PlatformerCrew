422a96e1900f7d642d2202d96fbbaa33a7cd3496
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-30 14:38:16 +0300

    add: script EnterCollisionComponent

---
+++ Assets/Scripts/Components/EnterCollisionComponent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace PixelCrew.Components
+{
+    public class EnterCollisionComponent : MonoBehaviour
+    {
+        [SerializeField] private string _tag;
+        [SerializeField] private EnterEvent _action;
+
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag(_tag))
+            {
+                _action?.Invoke(collision.gameObject);
+            }
+        }
+    }
+
+    [Serializable]
+    public class EnterEvent : UnityEvent<GameObject>
+    {
+
+    }
+}
