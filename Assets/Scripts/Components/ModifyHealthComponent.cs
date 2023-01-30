cb03500b8be48594d71a446bbb0327236cb681ef
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-01-30 14:38:51 +0300

    add: script ModifyHealthComponent

---
+++ Assets/Scripts/Components/ModifyHealthComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class ModifyHealthComponent : MonoBehaviour
+    {
+        [SerializeField] private int _hpDelta;
+
+        public void ApplyHealth(GameObject target)
+        {
+            var healthComponent = target.GetComponent<HealthComponent>();
+
+            if (healthComponent != null)
+            {
+                healthComponent.ApplyDamage(_hpDelta);
+            }
+        }
+    }
+}
