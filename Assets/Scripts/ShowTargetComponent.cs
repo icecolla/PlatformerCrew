324bebd1d72c47adece9babeb62c79bec1f617ee
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-02-22 13:28:18 +0300

    add: script ShowTargetComponent

---
+++ Assets/Scripts/ShowTargetComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class ShowTargetComponent : MonoBehaviour
+    {
+        [SerializeField] private CameraStateController _controller;
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _delay = 1f;
+
+        private void OnValidate()
+        {
+            if (_controller == null)
+            {
+                _controller = FindObjectOfType<CameraStateController>();
+            }
+        }
+
+        public void ShowTarget()
+        {
+            _controller.SetPosition(_target.position);
+            _controller.SetState(true);
+            Invoke(nameof(MoveBack), _delay);
+        }
+
+        private void MoveBack()
+        {
+            _controller.SetState(false);
+        }
+    }
+}
