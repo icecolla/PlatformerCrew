3552cdebbe23c86399c58a437c9d6e031eeb79f4
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-03-04 13:51:44 +0300

    fix: inverXScale in SpawnComponent

--- Assets/Scripts/Components/SpawnComponent.cs
+++ Assets/Scripts/Components/SpawnComponent.cs
@@ -6,12 +6,16 @@ namespace PixelCrew.Components
     {
         [SerializeField] private Transform _target;
         [SerializeField] private GameObject _prefab;
+        [SerializeField] private bool _invertXScale;
 
         [ContextMenu("Spawn")]
         public void Spawn()
         {
             var instance = Instantiate(_prefab, _target.position, Quaternion.identity);
-            instance.transform.localScale = _target.lossyScale;
+
+            var scale = _target.lossyScale;
+            scale.x *= _invertXScale ? -1 : 1; ;
+            instance.transform.localScale = scale;
         }
     }
 }
