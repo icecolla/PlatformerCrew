ef7f8fd9835a54fc403837cf307c3d8f245b1794
Vasiliy Sidorenko <v.sidorenko@kefirgames.com>
2023-03-05 18:51:54 +0300

    chg: refactor LayerCheck

--- Assets/Scripts/LayerCheck.cs
+++ Assets/Scripts/LayerCheck.cs
@@ -1,28 +1,11 @@
 ﻿using UnityEngine;
 
-namespace PixelCrew
+namespace PixelCrew.Components
 {
     public class LayerCheck : MonoBehaviour
     {
-        [SerializeField] private LayerMask _layer;
-        [SerializeField] private bool _isTouchingLayer;
-        private Collider2D _collider;
-
+        [SerializeField] protected LayerMask _layer;
+        [SerializeField] protected bool _isTouchingLayer;
         public bool IsTouchingLayer => _isTouchingLayer;
-
-        private void Awake()
-        {
-            _collider = GetComponent<Collider2D>();
-        }
-
-        private void OnTriggerStay2D(Collider2D collision)
-        {
-            _isTouchingLayer = _collider.IsTouchingLayers(_layer);
-        }
-
-        private void OnTriggerExit2D(Collider2D collision)
-        {
-            _isTouchingLayer = _collider.IsTouchingLayers(_layer);
-        }
     }
 }
